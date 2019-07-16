28a8e86a7c242743c25bcf2bf33c3751f96b483e
OrojiHacker <52883876+OrojiHacker@users.noreply.github.com>
2019-07-16 13:24:30 +0700

    Add files via upload

---
+++ you.py
@@ -0,0 +1,371 @@
+T  = "\033[90;1m" # dark
+f  = "\033[96;1m" # Blue aqua
+m  = "\033[34;1m" # Blue light
+Y  = "\033[33;1m" # Yellow light
+G  = "\033[32;1m" # Green light
+W  = "\033[0;1m"  # White light
+R  = "\033[31;1m" # Red light
+p = "\033[35;1m"
+import time
+import random
+import sys
+import os
+from time import sleep as timeout
+def Oroji():
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+        curdir = os.getcwd()
+################################################################
+def Number():
+     print G+"GOOO Game"
+     A = random.randint(1,10)
+     B = input("Number you:")
+     if B == 00:
+         sys.exit()
+     elif B == A:
+         print ("Yes Number{}".format(A))
+     else:
+         print ("No Number {}".format(A))
+#################################################################
+def Hacker():
+        print f+"tonHacker"
+        os.system("clear")
+        os.system("figlet tonHacker")
+        print "[01]> HPAS1369                [02]> A-Rat"
+        print "[03]> SQLMap                  [04]> Vbug"
+        print "[05]> Black Hydra             [06]> Weeman"
+        print "[07]> Xerxes                  [08]> Emalibomb"
+        print "[09]> Hammer                  [10]> Xshell"
+        print "[11]> wphunter                [12]> Pntddos"
+        print "[00]Exit"
+        H = raw_input("tonHacker :")
+
+
+        def HPAS():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y ")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/DedSecCyber/HPAS1369.git")
+             Hacker()
+        def ARat():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet")
+             os.system("git clone https://github.com/Xi4u7/A-Rat")
+             Hacker()
+        def sqlmap():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/sqlmapproject/sqlmap")
+             Hacker()
+        def vbug():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/jota01234/Vbug.git")
+             Hacker()
+        def BH():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/Gameye98/Black-Hydra.git")
+             Hacker()
+        def ww():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/evait-security/weeman.git")
+             os.system("clear")
+             Hacker()
+        def xearx():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system('git clone https://github.com/zanyarjamal/xerxes')
+             Hacker()
+        def EMB():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/Ha3MrX/Emailbomb.git")
+             Hacker()
+        def hammer():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/cyweb/hammer.git")
+             Hacker()
+        def xshell():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install figlet -y")
+             os.system("git clone https://github.com/Ubaii/Xshell")
+             Hacker()
+        def wphunter():
+             os.system("pkg install git -y")
+             os.system("pkg install python python2 -y")
+             os.system("pkg install php -y")
+             os.system("git clone https://github.com/Jamalc0m/wphunter")
+             Hacker()
+        def panddos():
+             os.system("apt-get upgrade -y && apt-get update -y")
+             os.system("apt-get install git -y")
+             os.system("apt-get install python python2 -y")
+             os.system("git clone https://github.com/Hydra7/Planetwork-DDOS.git")
+             Hacker()
+	if H == "1" or H == "01":
+              HPAS()
+        elif H == "2" or H == "02":
+              ARat()
+        elif H == "3" or H == "03":
+              sqlmap()
+        elif H == "4" or H == "04":
+              vbug()
+        elif H == "5" or H == "05":
+              BH()
+        elif H == "6" or H == "06":
+              ww()
+        elif H == "7" or H == "07":
+              xearx()
+        elif H == "8" or H == "08":
+             EMB()
+        elif H == "9" or H == "09":
+              hammer()
+        elif H == "10":
+              xshell()
+        elif H == "11":
+              wphunter()
+        elif H == "12":
+              panddos()
+        elif H == "00":
+            sys.exit()
+        else:
+             Hacker()
+
+
+#################################################################
+def pp():
+        def G13():
+                print "p/o"
+                G13 = raw_input("p/o :")
+                if G13 == "p":                                                                                                                                     pp()
+                elif G13 == "o":
+                        os.system("clear")
+                else:
+                     G13()
+        print f+"          uuuuuuuu     uuuuu"
+        print "            uuuuuuuu     uuuuu"
+        print "            uuuuuuuuu    uuuuu"
+        print "            uuuuuuuuuu   uuuuuu"
+        print "           uuuuuuuuuuu  uuuuuu"
+        print "           uuuuuu  uuu  uuuuuuu"
+        print "          uuuuuu       uuuuuuu"
+        print "          uuuuuuu      uuuuuuu"
+        print "          uffffffffffffffffffu"
+        print "          ffffffffffffffffffff"
+        print "         fffffffffffffffffffffff"
+        print "       fffffffffffffffffffffffffff"
+        print "      ffffffffffffffffffffffffffffff"
+        print "     ffffffffffffffffffffffffffffffff"
+        print "    ffff [          fffffff      ]ffff"
+        print "    fff  [        fffffffff      ]  fffff"
+        print "     ff  [  (??)      ff    (??) ]   fffff"
+        print "     fff [            ff         ]   ffff"
+        print "     fff [           ff         ]  ffff"
+        print "      fff [                    ]  ff"
+        print "       fff [      _______     ]"
+        print "        fff [                ]"
+        print "       ffffff $$$$$$$$@@@@@@@@"
+        print "       ffffffooooooooooooooooool"
+        print "       ffffff               j   l"
+        print "       fff j                 j   l"
+        print "      ffffj                   j   l"
+        print "       l   j                   j l"
+        print "        l   j                 j l"
+        print "         l   j               j l"
+        print "          l  j              j l"
+        print "           l j              j"
+        print "             j              j"
+        print "             j              j"
+        print "           ///////////////////"
+        print "         //////////////////////"
+        print "       /////////////////////////"
+        print "      //////////////////////////"
+        print "     ///////////////////////////"
+        print "    /////////////////////////////"
+        print "0010101001100101010010101010010111001010100101010"
+        os.system("figlet Master want")
+        M = raw_input("Master :")
+        if M == "love":
+            os.system("figlet 13loveMaster")
+            G13()
+        elif M == "00":
+         sys.exit()
+        else:
+            os.system("figlet 13REEOE")
+            G13()
+#################################
+def BT():
+        os.system("clear")
+        print R+"                      $-$-@@$-$@"
+        print "                                @$$@"
+        print "$-$-$@@$--$$-@-@-@@$$$$$@@----$-@@@@-$"
+        print "@@@@@@@@@@$$$@@$$$@@@@$$@@--@@@--@@@@@-$----"
+        print "$@@$$-$@@-$-@@@@$-$$-@-@-@@@-$--@-@@$@"
+        print "                                $$@@"
+        print "                       $$@@-$-$-@"
+        print
+        os.system("php BT.php")
+#################################################################
+def BB():
+        print Y+"BB"
+        os.system("clear")
+        os.system("figlet BB")
+        print "#####################################"
+        print "[01]+"
+        print
+        print "[02]-"
+        print
+        print "[03]*"
+        print
+        print "[04]/"
+        print
+        print "[00]Exit"
+        c = raw_input("Number :")
+        def q():
+             os.system("clear")
+             os.system("figlet +")
+             A = input("Number :")
+             B = input("Number :")
+             print (A + B)
+             timeout(5)
+             BB()
+        def w():
+             os.system("clear")
+             os.system("figlet -")
+             A = input("Number :")
+             B = input("Number :")
+             print (A - B)
+             timeout(5)
+             BB()
+        def e():
+             os.system("clear")
+             os.system("figlet *")
+             A = input("Number :")
+             B = input("Number :")
+             print (A * B)
+             timeout(5)
+             BB()
+        def r():
+             os.system("clear")
+             os.system("figlet /")
+             A = input("Number :")
+             B = input("Number :")
+             print (A / B)
+             timeout(5)
+             BB()
+        if c == "1" or c == "01":
+              q()
+        elif c == "2" or c == "02":
+                w()
+        elif c == "3" or c == "03":
+                e()
+        elif c == "4" or c == "04":
+                r()
+        elif c == "00":
+         sys.exit()
+        else:
+             BB()
+#################################################################
+def gg():
+        os.system("clear")
+        print p+"        uuuuuuuu     uuuuu"
+        print "          uuuuuuuuu    uuuuu"
+        print "          uuuuuuuuuu   uuuuuu"
+        print "          uuuuuuuuuuu  uuuuuu"
+        print "          uuuuuu  uuu  uuuuuuu"
+        print "          uuuuuu       uuuuuuu"
+        print "          uuuuuuu      uuuuuuu"
+        print "          uffffffffffffffffffu"
+        print "          ffffffffffffffffffff"
+        print "         fffffffffffffffffffffff"
+        print "       fffffffffffffffffffffffffff"
+        print "      ffffffffffffffffffffffffffffff"
+        print "     ffffffffffffffffffffffffffffffff"
+        print "    ffff [          fffffff      ]ffff"
+        print "    fff  [        fffffffff      ]  fffff"
+        print "    ff  [  ----      ff    (()) ]   fffff"
+        print "    fff [            ff         ]   ffff"
+        print "     fff [           ff         ]  ffff"
+        print "      fff [     \__________/   ]  ff"
+	print "       fff [                  ]"
+        print "        fff [                ]"
+        print "       ffffff $$$$$$$$@@@@@@@@"
+        print "       ffffffooooooooooooooooool     ???????????????"
+        print "       ffffff               j   l    ?             ?"
+        print "       fff j                 j   l   ?  SNAKE 13   ?"
+        print "      ffffj                   j   l  ?             ?"
+        print "       l   j                   j l   ????????????????"
+        print "        l   j                 j l"
+        print "         l   j               j l"
+        print "          l  j              j l"
+        print "           l j              j"
+        print "             j              j"
+        print "             j              j"
+        print "           ///////////////////"
+        print "         //////////////////////"
+        print "       /////////////////////////"
+        print "      //////////////////////////"
+        print "     ///////////////////////////"
+        print "    /////////////////////////////"
+        print "0010101001100101010010101010010111001010100101010"
+        print " Hack"
+        print "Am Oroji"
+        print
+        print "[01]>pp13         [02]>tonHacker         "
+        print
+        print "[03]>spmmerBT     [04]>spmmerSMS         "
+        print
+        print "[05]>BB+          [06]>DDOS              "
+        print
+        print "[07]>gameNumber   [08]>Gmail             "
+        print
+        print "[00]>Exit                                "
+
+
+        A = raw_input("Master want :")
+        if A == "1" or A == "01":
+            pp()
+        elif A == "2" or A == "02":
+              Hacker()
+        elif A == "3" or A == "03":
+              BT()
+        elif A == "4" or A == "04":
+              h = raw_input("-h :")
+              os.system("python2 sys.py"(h))
+        elif A == "5" or A == "05":
+              BB()
+        elif A == "6" or A == "06":
+              os.system("clear")
+              os.system("figlet snke DDOS")
+              ip = raw_input("IP Address : ")
+              port = raw_input("Port       : ")
+              packet =raw_input("Packet     : ")
+              os.system("python2 DDOS.py%s %s %s" % (ip, port, packet))
+        elif A == "7" or A == "07":
+              Number()
+        elif A == '8' or A == '08':
+               os.system("clear")
+               os.system("figlet Gmail Brute Force")
+               email = raw_input("[*] Email : ")
+               word = raw_input("[*] Wordlist : ")
+               os.system("hydra -l %s -P %s -s 465 smtp.gmail.com smtp" % (email, word))
+               sys.exit()
+        elif A == "00":
+              sys.exit()
+        else:
+              os.system("clear")
+gg()
+#################################################################
+gg()
